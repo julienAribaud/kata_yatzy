73cfffebbf1d1a6cb0cac85b8a341efeae878599
JAD <julien.aribaud@gmail.com>
2020-05-16 17:49:57 +0200

    factorized pair , three and four of a kind with one function && add case 0 when called && rename

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -46,6 +46,16 @@ public class Yatzy {
         return counts;
     }
 
+    private int sumWantedIterationOfDiceValue(int numberWanted) {
+        int[] counts = countDiceValues();
+
+        for (int i = counts.length - 1; i >= 0; i--)
+            if (counts[i] >= numberWanted)
+                return (i + 1) * numberWanted;
+
+        return 0;
+    }
+
     public int ones() { return sumValueCalled(1); }
     public int twos(){
         return sumValueCalled(2);
@@ -58,15 +68,6 @@ public class Yatzy {
     }
     public int fives() { return sumValueCalled(5); }
     public int sixes() { return sumValueCalled(6); }
-
-    public int pair() {
-        int[] counts= countDiceValues();
-
-        for (int i=counts.length-1;i>=0;i--)
-           if(counts[i]>=2)
-               return (i+1)*2;
-        return 0;
-    }
 
     public int two_pair() {
         int[] counts= countDiceValues();
@@ -86,34 +87,16 @@ public class Yatzy {
         return 0;
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+    public int pair() {
+        return sumWantedIterationOfDiceValue(2);
     }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
+    public int three_of_a_kind() {
+        return sumWantedIterationOfDiceValue(3);
+    }
+
+    public int four_of_a_kind() {
+        return sumWantedIterationOfDiceValue(4);
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -84,18 +84,19 @@ public class YatzyTest {
     }
 
     @Test
-    public void three_of_a_kind() 
-    {
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
-        assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
+    public void three_of_a_kind_called() {
+        assertEquals(9, new Yatzy(3,3,3,4,5).three_of_a_kind());
+        assertEquals(15, new Yatzy(5,3,5,4,5).three_of_a_kind());
+        assertEquals(9, new Yatzy(3,3,3,3,5).three_of_a_kind());
+        assertEquals(0, new Yatzy(3,2,1,3,5).three_of_a_kind());
     }
 
     @Test
-    public void four_of_a_knd() {
-        assertEquals(12, Yatzy.four_of_a_kind(3,3,3,3,5));
-        assertEquals(20, Yatzy.four_of_a_kind(5,5,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
+    public void four_of_a_kind_called() {
+        assertEquals(12, new Yatzy(3,3,3,3,5).four_of_a_kind());
+        assertEquals(20, new Yatzy(5,5,5,4,5).four_of_a_kind());
+        assertEquals(12, new Yatzy(3,3,3,3,3).four_of_a_kind());
+        assertEquals(0, new Yatzy(3,3,2,1,3).four_of_a_kind());
     }
 
     @Test
