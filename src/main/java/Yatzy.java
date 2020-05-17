ad6bb9467a923dafd7654a7cb7642d8a04c92e63
JAD <julien.aribaud@gmail.com>
2020-05-17 13:07:54 +0200

    rename && replace && add calculValueByTiùe

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -26,22 +26,6 @@ public class Yatzy {
         return sum;
     }
 
-    public int yatzy() {
-        for (int count : countDiceValues())
-            if (count == NUMBER_OF_DICE)
-                return YATZY_VALUE;
-        return DEFAULT_VALUE_INVALID;
-    }
-
-    private int sumValueCalled(int valueCalled){
-        int sum=0;
-        for(int dice:dices){
-            if(dice==valueCalled)
-                sum+=valueCalled;
-        }
-        return sum;
-    }
-
     private int[] countDiceValues(){
         int[] counts = new int[MAX_VALUE];
 
@@ -51,57 +35,72 @@ public class Yatzy {
         return counts;
     }
 
-    private int multiplyWantedIteration(int numberWanted) {
-        int[] counts = countDiceValues();
-
-        for (int diceValue = counts.length - 1; diceValue >= 0; diceValue--)
-            if (counts[diceValue] >= numberWanted)
-                return (diceValue + 1) * numberWanted;
-
+    public int yatzy() {
+        for (int count : countDiceValues())
+            if (count == NUMBER_OF_DICE)
+                return YATZY_VALUE;
         return DEFAULT_VALUE_INVALID;
     }
 
+    private int sumValueCalled(int valueCalled) {
+        int sum = 0;
+        for (int dice : dices) {
+            if (dice == valueCalled)
+                sum += valueCalled;
+        }
+        return sum;
+    }
+
     public int ones() { return sumValueCalled(1); }
-    public int twos(){
-        return sumValueCalled(2);
-    }
-    public int threes(){
-        return sumValueCalled(3);
-    }
-    public int fours(){
-       return sumValueCalled(4);
-    }
+    public int twos() { return sumValueCalled(2); }
+    public int threes() { return sumValueCalled(3); }
+    public int fours() { return sumValueCalled(4); }
     public int fives() { return sumValueCalled(5); }
     public int sixes() { return sumValueCalled(6); }
+
+    private int calculValueByTimes(int value,int numberOfTimes){
+        return (value+1)*numberOfTimes;
+    }
 
     public int two_pair() {
         int[] counts= countDiceValues();
 
-        boolean gotDoubleYet=false;
-        int previousDoubleSum=0;
+        boolean gotPairYet=false;
+        int previousPoints=0;
 
         for (int diceValue=0;diceValue<counts.length;diceValue++) {
             if (counts[diceValue] >= 2) {
-                if (gotDoubleYet) {
-                    return (diceValue + 1) * 2 + previousDoubleSum;
+                int points=calculValueByTimes(diceValue, 2);
+                if (gotPairYet) {
+                    return points + previousPoints;
                 }
-                previousDoubleSum = (diceValue + 1) * 2;
-                gotDoubleYet = true;
+                previousPoints = points;
+                gotPairYet = true;
             }
         }
         return DEFAULT_VALUE_INVALID;
     }
 
+    private int searchAndCalculSameValueNTimes(int numberOfTime) {
+        int[] counts = countDiceValues();
+
+        for (int diceValue = counts.length - 1; diceValue >= 0; diceValue--)
+            if (counts[diceValue] >= numberOfTime)
+                return calculValueByTimes(diceValue,numberOfTime);
+
+        return DEFAULT_VALUE_INVALID;
+    }
+
     public int pair() {
-        return multiplyWantedIteration(2);
+        return searchAndCalculSameValueNTimes(2);
     }
 
     public int three_of_a_kind() {
-        return multiplyWantedIteration(3);
+        return searchAndCalculSameValueNTimes(3);
     }
 
     public int four_of_a_kind() {
-        return multiplyWantedIteration(4);
+        return searchAndCalculSameValueNTimes(4);
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
@@ -143,24 +142,26 @@ public class Yatzy {
     public int fullHouse() {
         int[] counts= countDiceValues();
 
-        boolean gotDoubleYet=false;
+        boolean gotPairYet=false;
         boolean gotTrippleYet=false;
-        int previousSum=0;
+        int previousPoints=0;
 
         for (int diceValue=0;diceValue<counts.length;diceValue++) {
             if (counts[diceValue] == 3) {
-                if (gotDoubleYet) {
-                    return (diceValue + 1) * 3 + previousSum;
+                int points=calculValueByTimes(diceValue, 3);
+                if (gotPairYet) {
+                    return points + previousPoints;
                 }
-                previousSum = (diceValue + 1) * 3;
+                previousPoints = points;
                 gotTrippleYet = true;
 
             }else if(counts[diceValue] == 2){
+                int points=calculValueByTimes(diceValue, 2);
                 if (gotTrippleYet) {
-                    return (diceValue + 1) * 2 + previousSum;
+                    return points + previousPoints;
                 }
-                previousSum = (diceValue + 1) * 2;
-                gotDoubleYet=true;
+                previousPoints = points;
+                gotPairYet=true;
             }
         }
         return DEFAULT_VALUE_INVALID;
